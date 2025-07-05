d3bc63ad918d84b0c209950cc96fc0cd41198bb8
Mohamed-Eid01 <mohamedaid8331@gmail.com>
2025-07-05 16:38:33 +0300

    react project

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+// src/components/Dashboard.js
+import React from "react";
+import "../styles/dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <h2>Dashboard</h2>
+      <p>You are logged in!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
